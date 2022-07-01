09ae12d50c7f34cc8e73786f9ce688832017e4ed
Hyunsoo Kim <67745406+KimHyunsoo-ship-it@users.noreply.github.com>
2022-07-02 06:45:20 +0900

    App.tsx에서 component 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 import GlobalStyles from './themes/globalStyles';
-import ContentLogo from './themes/contentLogoStyle';
-import AwardStore from './themes/awardStoreStyle';
-import AwardApple from './themes/awardAppleStyle';
+import ContentLogo from './components/ContentLogo';
+import Awards from './components/Awards';
 import Metrics from './components/Metrics';
 
 const MainStyle = styled.div`
@@ -15,7 +14,7 @@ const MainStyle = styled.div`
   margin-top: 140px;
 `;
 
-const Left = styled.div`
+const LeftMargin = styled.div`
   margin-left: 623px;
 `;
 
@@ -25,23 +24,14 @@ const App = () => {
       <GlobalStyles />
 
       <MainStyle>
-          <ContentLogo>
-            2021년 12월 기준
-          </ContentLogo>
+        
+          <ContentLogo />
 
-          <Left>
+          <LeftMargin>
             <Metrics />
-            <AwardStore>
-              2018 구글 플레이스토어
-              <br />
-              올해의 앱 최우수상 수상
-            </AwardStore>
-            <AwardApple>
-              2018 애플 앱스토어
-              <br />
-              오늘의 여행앱 선정
-            </AwardApple>
-          </Left>
+            <Awards />
+          </LeftMargin>
+
       </MainStyle>
     </>
   );
---
+++ src/components/ContentLogo.tsx
@@ -0,0 +1,13 @@
+import ContentLogo from '../themes/contentLogoStyle';
+
+const Logo = () => {
+  return (
+    <>
+      <ContentLogo>
+        2021년 12월 기준
+      </ContentLogo>
+    </>
+  );
+}
+
+export default Logo;
