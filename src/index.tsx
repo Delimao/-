1605cbfc96c63107dfb1e50a6c4c0beba3636cf1
Hyunsoo Kim <67745406+KimHyunsoo-ship-it@users.noreply.github.com>
2022-07-05 01:28:05 +0900

    코드 정리 및 불필요한 tsx -> ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-/**
- * react-query : QueryClient, QueryClientProvider
- * react-query/devtools : ReactQueryDevtools
- * 
- * QueryClient 사용 위해 queryClient 선언
- * **/
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-const queryClient = new QueryClient();
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-/** 
- * QueryClientProvider 후, 'App.tsx' rendering
- * **/
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/paths/ImagesPath.ts
@@ -0,0 +1,5 @@
+import Store from '../images/play-store2x.png';
+import Apple from '../images/badge-apple4x.png';
+import ContentLogo from '../images/triple2x.png';
+
+export { Store, Apple, ContentLogo };
